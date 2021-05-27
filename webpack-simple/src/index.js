f33de293db47505558d5fb1c79167de62e6feec4
gitspawn <volosgoto@gmail.com>
2021-05-27 18:49:19 +0300

    add lesson 23

---
+++ webpack-simple/src/index.js
@@ -0,0 +1,16 @@
+import "./css/base.scss";
+
+// import menuTemplate from './tempaltes/menu.handlebars'
+import menuTemplate from './tempaltes/menu.hbs'
+import menuData from './js/menu-data'
+import refs from './js/refs'
+
+
+
+let markup = menuTemplate(menuData)
+refs.root.innerHTML = markup
+
+
+
+
+// npm i handlebars handlebars-loader --save
---
+++ webpack-simple/src/js/menu-data.js
@@ -0,0 +1,8 @@
+let menuData = {
+  title: 'Pizza Carbonara',
+  items: ['cheese', 'pomato', 'onion', 'mazarella']
+}
+// let menuData = 'Pizza'
+
+
+export default menuData;
