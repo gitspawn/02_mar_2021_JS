710e3de78e833fb565b4eb9ee642b30e7c4672ce
gitspawn <volosgoto@gmail.com>
2021-05-27 08:44:02 +0300

    add lesson 23

---
+++ Lesson_22, 23. Ajax,Fetch, Cors/part_2/app.js
@@ -0,0 +1,10 @@
+/*
+Thank you for registering with the open platform.
+A new key has been created for you: 3534a47c-f615-488a-ac2b-1cbb3a5481ee
+You can try this key by accessing https://content.guardianapis.com/search?api-key=3534a47c-f615-488a-ac2b-1cbb3a5481ee in your browser.
+*/
+
+// https://open-platform.theguardian.com/documentation/
+// Example: https://content.guardianapis.com/search?page=2&q=debate&api-key=test
+
+// let API_KEY = '3534a47c-f615-488a-ac2b-1cbb3a5481ee'
