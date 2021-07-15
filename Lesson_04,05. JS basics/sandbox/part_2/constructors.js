7803a83ebd64c3c144daa5113d7c3593cc345051
gitspawn <volosgoto@gmail.com>
2021-07-15 18:50:24 +0300

    add js basics

---
+++ Lesson_04,05. JS basics/sandbox/part_2/constructors.js
@@ -0,0 +1,32 @@
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+// Object
+
+
+// let userName = 'Sara';
+// console.log(userName.constructor)
+// console.log(userName)
+
+// let num = 30
+// console.log(num)
+// console.log(num.constructor)
+
+// let isAdmin = true
+// console.log(isAdmin.constructor)
+
+
+
+// let userName = 'Sara';
+// let userName = new String('Sara')
+// console.log(userName.constructor)
+
+
+// let num = 30
+// let num = new Number(30)
+// console.log(num)
+// console.log(num.constructor)
